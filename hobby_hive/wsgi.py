f2c985b4b003bd4b85af99f299b42ae2e9da0e0a
Shriranjan Patil <82278266+ranjan2601@users.noreply.github.com>
2024-10-28 19:22:48 -0400

    update files

--- hobby_hive/wsgi.py
+++ hobby_hive/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+settings_module = 'hobby_hive.development' if 'WEBSITE_HOSTNAME' in os.environ else 'hobby_hive.settings'
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hobby_hive.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hobby_hive.settings')
+    settings_module = 'hobby_hive.development' if 'WEBSITE_HOSTNAME' in os.environ else 'hobby_hive.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
