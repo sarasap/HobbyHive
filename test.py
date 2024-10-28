50a636b4d0e59ed638ae055ce0099f93d8d17e6e
Shriranjan Patil <82278266+ranjan2601@users.noreply.github.com>
2024-10-28 18:43:57 -0400

    add requirements.txt

---
+++ test.py
@@ -0,0 +1,7 @@
+import dj_rest_auth, django, rest_framework, rest_framework_simplejwt, rest_framework_social_oauth2, corsheaders
+
+print(django.__version__)
+print(rest_framework.__version__)
+print(rest_framework_simplejwt.__version__)
+print(rest_framework_social_oauth2.__version__)
+print(corsheaders.__version__)
