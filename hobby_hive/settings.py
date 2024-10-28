f1795bfb4966388b21ff7b04f5f93b8496223fc2
Shriranjan Patil <82278266+ranjan2601@users.noreply.github.com>
2024-10-27 20:10:49 -0400

    add secret key

--- hobby_hive/settings.py
+++ hobby_hive/settings.py
@@ -23,7 +23,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=_qh8snpx)hvvix(o12dat)&cr0vz!ap+ro@1xs#09j4ma1v)m'
+# SECRET_KEY = 'django-insecure-=_qh8snpx)hvvix(o12dat)&cr0vz!ap+ro@1xs#09j4ma1v)m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
