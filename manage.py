eb5e757c30fa1283689c293b66244348283b805c
Shriranjan Patil <82278266+ranjan2601@users.noreply.github.com>
2024-10-28 22:45:40 -0400

    update files

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    settings_module = 'hobby_hive.development' if 'WEBSITE_HOSTNAME' in os.environ else 'hobby_hive.settings'
+    settings_module = 'hobby_hive.production' if 'WEBSITE_HOSTNAME' in os.environ else 'hobby_hive.settings'
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
