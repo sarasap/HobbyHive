31fb88ff38f8cfa1a143518b0fe7673c612e612e
Shriranjan Patil <82278266+ranjan2601@users.noreply.github.com>
2024-10-29 13:05:27 -0400

    Update file

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getToken, removeToken } from './auth';
 
-const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+const baseURL = process.env.REACT_APP_API_URL;
 
 const axiosInstance = axios.create({
   baseURL,
