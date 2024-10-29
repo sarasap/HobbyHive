35bcb6ba58aa07852ab71eed3eaf31337fa97b46
Shriranjan Patil <82278266+ranjan2601@users.noreply.github.com>
2024-10-29 13:23:09 -0400

    Update file

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 import { getToken, removeToken } from './auth';
 
-const baseURL = process.env.REACT_APP_API_URL;
+console.log('Node ENV:', process.env.NODE_ENV);
+console.log('API URL:', process.env.REACT_APP_API_URL);
+
+const baseURL = process.env.NODE_ENV === 'production' 
+  ? 'https://hobbyhive-dbhtapemgpdeayc7.northcentralus-01.azurewebsites.net'
+  : 'http://localhost:8000';
+
+console.log('Using baseURL:', baseURL);
+
 
 const axiosInstance = axios.create({
   baseURL,
