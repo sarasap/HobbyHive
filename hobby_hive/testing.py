8ddfcd7b06fa11fd97ffcae8a85431996482e8d1
Shriranjan Patil <82278266+ranjan2601@users.noreply.github.com>
2024-11-26 11:05:42 -0500

    Update files

--- hobby_hive/testing.py
+++ hobby_hive/testing.py
@@ -3,6 +3,69 @@ from .settings import *  # Import all default settings
 # Test-specific configurations
 DEBUG = True
 
+ALLOWED_HOSTS = [
+    "https://hobbyhive-dbhtapemgpdeayc7.northcentralus-01.azurewebsites.net",
+    "localhost",
+    "127.0.0.1",
+]
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'hhMain',
+    'hhPosts',
+]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://hobbyhive-dbhtapemgpdeayc7.northcentralus-01.azurewebsites.net",
+]
+
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+
 # Use Azure PostgreSQL DB for testing
 DATABASES = {
     'default': {
